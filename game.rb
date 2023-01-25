2eb913eedfe12ea9dd4966a02acb544c763287c6
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-25 01:54:18 -0500

    added call and response to question

--- game.rb
+++ game.rb
@@ -13,4 +13,12 @@ class Game
     @question = "What is #{num1} #{operator} #{num2}?"
     @answer = num1.send(operator, num2)
   end
+
+  def check_answer(player_answer)
+    if player_answer == @answer
+      puts "Correct!"
+    else
+      puts "Wrong! The correct answer is #{@answer}! You lose a life!"
+    end
+  end
 end
--- play.rb
+++ play.rb
@@ -4,6 +4,8 @@ require './game.rb'
 player_1 = Player.new
 player_2 = Player.new
 game = Game.new
+
 game.generate_question
 puts game.question
-puts game.answer
+answer = gets.chomp.to_i
+game.check_answer(answer)
