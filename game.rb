d97bb1b1775e05559cb9da869c85fd5281eb4745
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-25 01:31:10 -0500

    intial commit

--- game.rb
+++ game.rb
@@ -1,5 +1,16 @@
 class Game 
+  attr_accessor :question
+
   def initialize
     puts "Game initialized"
   end
+
+  def generate_question
+    num1 = 1
+    num2 = 2
+    operator = +
+    return "What is ${num1} ${operator} ${num2}?"
+  end
+
+
 end
