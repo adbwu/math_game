8c3f389b5ea8a51d6f3b1820cc23524ea151b759
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-25 01:41:04 -0500

    created function to produce question and answer

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 class Game 
-  attr_accessor :question
+  attr_accessor :question, :answer
 
   def initialize
     puts "Game initialized"
@@ -8,9 +8,8 @@ class Game
   def generate_question
     num1 = 1
     num2 = 2
-    operator = +
-    return "What is ${num1} ${operator} ${num2}?"
+    operator = "+"
+    @question = "What is #{num1} #{operator} #{num2}?"
+    @answer = num1.send(operator, num2)
   end
-
-
 end
--- play.rb
+++ play.rb
@@ -4,3 +4,6 @@ require './game.rb'
 player_1 = Player.new
 player_2 = Player.new
 game = Game.new
+game.generate_question
+puts game.question
+puts game.answer
